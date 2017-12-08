5c8be64277a481d8cf54a1c0facfaad931ec3e13
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2017-12-08 19:08:23 +0100

    Quick changes for java 9. Does not work with java <= 8.

--- protege-editor-core/src/main/java/org/protege/editor/core/platform/apple/ProtegeAppleApplication.java
+++ protege-editor-core/src/main/java/org/protege/editor/core/platform/apple/ProtegeAppleApplication.java
@@ -1,17 +1,20 @@
 package org.protege.editor.core.platform.apple;
 
-import org.protege.editor.core.ProtegeApplication;
-import org.protege.editor.core.ProtegeManager;
-import org.protege.editor.core.editorkit.EditorKit;
-import org.protege.editor.core.ui.about.AboutPanel;
-import org.protege.editor.core.ui.preferences.PreferencesDialogPanel;
-
+import java.awt.Desktop;
 import java.io.File;
 /*
 * Copyright (C) 2007, University of Manchester
 *
 *
 */
+
+import org.protege.editor.core.ProtegeApplication;
+import org.protege.editor.core.ProtegeManager;
+import org.protege.editor.core.editorkit.EditorKit;
+import org.protege.editor.core.ui.about.AboutPanel;
+import org.protege.editor.core.ui.preferences.PreferencesDialogPanel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Author: drummond<br>
@@ -21,8 +24,10 @@ import java.io.File;
  * Bio Health Informatics Group<br>
  * Date: Sep 19, 2008<br><br>
  */
-public class ProtegeAppleApplication extends AbstractAppleApplicationWrapper {
+public class ProtegeAppleApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(ProtegeAppleApplication.class);
+	
     private EditorKit eKit;
 
     private static ProtegeAppleApplication instance;
@@ -36,17 +41,27 @@ public class ProtegeAppleApplication extends AbstractAppleApplicationWrapper {
     }
 
 
-    private ProtegeAppleApplication() {
-    }
+	private ProtegeAppleApplication() {
+		Desktop application = Desktop.getDesktop();
+		application.setPreferencesHandler(event -> handlePreferencesRequest());
+		application.setAboutHandler(event -> handleAboutRequest());
+		application.setOpenFileHandler(event -> {
+			File file = event.getFiles().get(0);
+			try {
+				editFile(file.getAbsolutePath());
+			} catch (Exception e) {
+				logger.error("invalid file: {}", file);
+			}
+		});
+		application.setQuitHandler((event, response) -> handleQuitRequest());
+	}
 
 
     public void setEditorKit(EditorKit eKit){
         this.eKit = eKit;
-        setEnabledPreferencesMenu(eKit != null);
     }
     
-    @Override
-    protected void editFile(String fileName) throws Exception {
+    protected void editFile(String fileName) throws Exception {    	    
         ProtegeManager.getInstance().getApplication().editURI(new File(fileName).toURI());
     }
 
